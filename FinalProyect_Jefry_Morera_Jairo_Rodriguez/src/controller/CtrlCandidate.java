172ba9db4d0206c93739b1f41d59c0f43407f46b
Jefry Morera <jefrymoreraespinoza@gmail.com>
2023-11-06 23:37:05 -0600

    method for multiple panes done

--- FinalProyect_Jefry_Morera_Jairo_Rodriguez/src/controller/CtrlCandidate.java
+++ FinalProyect_Jefry_Morera_Jairo_Rodriguez/src/controller/CtrlCandidate.java
@@ -2,7 +2,20 @@
 package controller;
 
 import Controller.Validation;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.net.URL;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import model.candidate;
@@ -88,5 +101,58 @@ public class CtrlCandidate {
         idNumber.setText("");
         age.setText("");
         politicParty.setText("");
+    }
+    private void displayCantidates(List<candidate> candidates, JScrollPane scrollPane) {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        for (candidate candi : candidates) {
+            JPanel candidatePanel = new JPanel();
+            candidatePanel.setPreferredSize(new Dimension(1224, 300));
+            candidatePanel.setBorder(BorderFactory.createTitledBorder(candi.getName()));
+            candidatePanel.setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+
+            // JLabel for ID
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            candidatePanel.add(new JLabel("ID: " + candi.getId()), gbc);
+
+            // JLabel for Name
+            gbc.gridy++;
+            candidatePanel.add(new JLabel("Nombre: " + candi.getName()), gbc);
+
+            // JLabel for Category
+            gbc.gridy++;
+            candidatePanel.add(new JLabel("Cedula: " + candi.getIdNumber()), gbc);
+
+            // JLabel for Instructions
+            gbc.gridy++;
+            candidatePanel.add(new JLabel("Edad: " + candi.getAge()), gbc);
+            
+
+            gbc.gridy++;
+            candidatePanel.add(new JLabel("Partido Politico: " + candi.getPoliticParty()),gbc);
+
+            /** JLabel for Image
+            gbc.gridy++;
+            try {
+                URL url = new URL(cocktail.getImageUrl());
+                ImageIcon icon = new ImageIcon(url);
+                Image originalImage = icon.getImage();
+                Image resizedImage = originalImage.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+                ImageIcon resizedIcon = new ImageIcon(resizedImage);
+
+                JLabel imageLabel = new JLabel(resizedIcon);
+
+                cocktailPanel.add(imageLabel, gbc);
+            } catch (Exception e) {
+                cocktailPanel.add(new JLabel("Image not available"), gbc);
+            }*/
+
+            mainPanel.add(candidatePanel);
+        }
+
+        scrollPane.setViewportView(mainPanel);
     }
 }
---
+++ Proyect2_IIIC/src/controller/Proyect2_IIIC.java
@@ -0,0 +1,14 @@
+package controller;
+
+/**
+ *
+ * @author jefry
+ */
+public class Proyect2_IIIC {
+
+ 
+    public static void main(String[] args) {
+        
+    }
+    
+}
